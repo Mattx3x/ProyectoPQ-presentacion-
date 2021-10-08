d1a1147da179484d862fa0b27bd8f24830cf3511
Mat3x3 <elestudianteuni21@gmail.com>
2021-10-08 01:35:11 -0300

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,28 @@
+Nombre_Apellido = input("Porfavor ingrese nombre y apellido para poder ingresar ")
+print ("Bienvenido " + Nombre_Apellido)
+
+Edad = input("Ingrese su edad ")
+print ("De acuerdo tu edad es de " + Edad)
+
+Estatus = input("Por favor diganos algo sobre usted, a que se dedica ? (Estudiante o profesor )")
+print("Interesante asi que usted es... "+ Estatus)
+
+print ("Acontinuacion le haremos realizar unas pocas preguntas y calculos con el fin de saber en conocimiento esta basado ")
+
+cal1 = int(input ("3 + 3 ="))
+if sum1 == 6 :
+    print ("Excelente")
+else :
+    print ("Error...vuelve a verificar")
+
+preg1 = input("Las celulas son unidades pequenas , pero en la quimica hablamos de... ")
+if preg1 == ("atomos") :
+     print("Bien has acertado, sigue asi !")
+else :
+    print("Has fallado, verifica tu error")
+cal2,preg2 =("Bien hecho!","Mal hecho!")
+input("diganos cuanto es 2 más 2 : ")
+if input == ("2"):
+    print(cal2)
+els:
+    print(preg2)
