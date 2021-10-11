885152834a4f73d52a27b5a2284df4633dd2f7e9
Mat3x3 <elestudianteuni21@gmail.com>
2021-10-11 20:35:54 -0300

    Nuevos Cambio

--- main.py
+++ main.py
@@ -2,27 +2,34 @@ Nombre_Apellido = input("Porfavor ingrese nombre y apellido para poder ingresar 
 print ("Bienvenido " + Nombre_Apellido)
 
 Edad = input("Ingrese su edad ")
-print ("De acuerdo tu edad es de " + Edad)
+print ("Edad: " + Edad)
 
 Estatus = input("Por favor diganos algo sobre usted, a que se dedica ? (Estudiante o profesor )")
-print("Interesante asi que usted es... "+ Estatus)
+print("Estatus: "+ Estatus)
 
 print ("Acontinuacion le haremos realizar unas pocas preguntas y calculos con el fin de saber en conocimiento esta basado ")
 
-cal1 = int(input ("3 + 3 ="))
-if sum1 == 6 :
+cal1 = int(input ("(10*3) / (6-3) =( ∧2) :"))
+if cal1 == 100 :
     print ("Excelente")
+if cal1 ==[90,
+           95,
+           110,]  :
+    print("Casi lo has logrado, verifica de vuelta ")
 else :
-    print ("Error...vuelve a verificar")
+    input(print ("No lo has logrado, quieres que lo resolvamos paso a paso Y/N (y, para verificar con los pasos , N,para seguir intentando solo)"))
+# agregar funcion para simplificar, lo de arriba
 
 preg1 = input("Las celulas son unidades pequenas , pero en la quimica hablamos de... ")
-if preg1 == ("atomos") :
+if preg1 == ("atomos", "particulas") :
      print("Bien has acertado, sigue asi !")
 else :
-    print("Has fallado, verifica tu error")
-cal2,preg2 =("Bien hecho!","Mal hecho!")
-input("diganos cuanto es 2 más 2 : ")
-if input == ("2"):
-    print(cal2)
-els:
-    print(preg2)
+    input(print("Has errado , si quieres podemos enseñarte un texto en donde podras entender algo de el tema Y/N"))
+
+#preg2_1,preg2 =("Bien hecho!","Vuelve intentarlo, si quieres podemos mostrarte algo que te ayudará Y/N :")
+#input("Diganos la configuracion electronica del nitrogeno: ")
+#if input == ("1s2,2s2,3p3"):
+#    print(preg2_1)
+#else:
+#    print(preg2)
+#def number ( multiplicacion = a * b) :
